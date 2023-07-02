0be33637d621b59068ab713cdd9dc739a79644cc
idomshi <56523771+idomshi@users.noreply.github.com>
2023-07-02 16:52:07 +0900

    直線を描くコードを書きましたがしっかり間違えています

--- front/composables/useImage.ts
+++ front/composables/useImage.ts
@@ -23,8 +23,39 @@ export const useImage = (imageData: Ref<ImageData>) => {
     y: number,
     pressure: number
   }
+
   let isDrawing = false
   let previousePos: Position = { x: 0, y: 0, pressure: 0 }
+
+  const line = (pos1: Position, pos2: Position): void => {
+    const deltax = Math.round(pos2.x - pos1.x)
+    const deltay = Math.round(pos2.y - pos1.y)
+
+    if (deltax === 0) {
+      const intx = Math.round(pos1.x)
+      for (let inty = Math.round(Math.min(pos1.y, pos2.y)); inty <= Math.round(Math.max(pos1.y, pos2.y)); ++inty) {
+        const idx = (inty * width + intx) * 4
+        pixel[idx] = pixel[idx + 1] = pixel[idx+2] = 0
+        pixel[idx + 3] = 255
+      }
+      return
+    }
+
+    let error = 0
+    const deltaerr = Math.abs(deltay / deltax)
+    let inty = Math.round(pos1.y)
+    for (let intx = Math.round(Math.min(pos1.x, pos2.x)); intx <= Math.round(Math.max(pos1.x, pos2.x)); ++intx) {
+        const idx = (inty * width + intx) * 4
+        pixel[idx] = pixel[idx + 1] = pixel[idx+2] = 0
+        pixel[idx + 3] = 255
+        error += deltaerr
+        if (error >= 0.5) {
+          inty += 1
+          error -= 1.0
+        }
+    }
+  }
+
   const stroke = (pos: Position): void => {
     // console.log(pos)
 
@@ -33,10 +64,12 @@ export const useImage = (imageData: Ref<ImageData>) => {
         isDrawing = false
         console.log('drawing end')
       }
+      line(previousePos, pos)
+      previousePos = pos
 
     } else {
       isDrawing = true
-      // previousePos = pos
+      previousePos = pos
       const idx = (Math.round(pos.y) * width + Math.round(pos.x)) * 4
       pixel[idx] = pixel[idx + 1] = pixel[idx + 2] = 0
       pixel[idx + 3] = 255
