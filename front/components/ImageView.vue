cc4dda5a6f57d387a29366c2e74f78c3dc5c9633
idomshi <56523771+idomshi@users.noreply.github.com>
2023-07-08 13:36:21 +0900

    キャンバスを回転できるようにします

--- front/components/ImageView.vue
+++ front/components/ImageView.vue
@@ -21,6 +21,7 @@ const onWindowResize = () => {
 
 const left = ref(0)
 const top = ref(0)
+const angle = ref(Math.PI / 32)
 
 onMounted(() => {
   if (viewcanvas.value === undefined) throw new Error('canvasを初期化できませんでした');
@@ -32,7 +33,7 @@ onMounted(() => {
   window.addEventListener('resize', onWindowResize)
   onWindowResize()
   left.value = (cw.value - width) / 2
-  top.value = (ch.value - height)  / 2
+  top.value = (ch.value - height) / 2
 })
 
 onBeforeUnmount(() => {
@@ -83,25 +84,45 @@ const redraw = () => {
   const w = Math.min(cw.value, iw)
   const h = Math.min(ch.value, ih)
   viewctx.value?.clearRect(0, 0, cw.value, ch.value)
+
+  viewctx.value?.save()
+  const ox = left.value + iw / 2
+  const oy = top.value + ih / 2
+  const c = Math.cos(angle.value)
+  const s = Math.sin(angle.value)
+  const dx = c * ox + s * oy - iw / 2
+  const dy = -s * ox + c * oy - ih / 2
+  viewctx.value?.transform(c, s, -s, c, c * dx - s * dy, s * dx + c * dy)
   viewctx.value.fillStyle = 'rgb(192, 192, 192)'
-  viewctx.value?.fillRect(left.value, top.value, iw, ih)
+  viewctx.value?.fillRect(0, 0, iw, ih)
   viewctx.value.fillStyle = 'rgb(255, 255, 255)'
   for (let w = 0; w < iw; w += 8) {
     for (let h = 0; h < ih; h += 8) {
       if ((w & 8) === (h & 8)) continue
-      viewctx.value.fillRect(w + left.value, h + top.value, Math.min(iw, w + 8) - w, Math.min(ih, h + 8) - h)
+      viewctx.value.fillRect(w, h, Math.min(iw, w + 8) - w, Math.min(ih, h + 8) - h)
     }
   }
-  viewctx.value?.drawImage(buffcanvas.value, 0, 0, w, h, left.value, top.value, w, h)
+  viewctx.value?.drawImage(buffcanvas.value, 0, 0, w, h, 0, 0, w, h)
+  viewctx.value?.restore()
 }
 
 watchEffect(redraw)
+
+const incAngle = () => {
+  angle.value += 0.01
+}
+
+const decAngle = () => {
+  angle.value -= 0.01
+}
 </script>
 
 <template>
   <div class="w-full h-screen flex flex-col">
     <div class="p-2 flex flex-row gap-2 bg-slate-200">
       <button @click="modify" class="px-4 h-8 bg-slate-300 border-2 border-slate-400 rounded">redraw</button>
+      <button @click="incAngle" class="px-4 h-8 bg-slate-300 border-2 border-slate-400 rounded">+</button>
+      <button @click="decAngle" class="px-4 h-8 bg-slate-300 border-2 border-slate-400 rounded">-</button>
     </div>
     <div class="h-full bg-slate-100">
       <canvas ref="viewcanvas" class="w-full h-full" @pointerdown="dragstart" @pointermove="dragmove"
